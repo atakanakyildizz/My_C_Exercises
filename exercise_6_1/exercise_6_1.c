147e7ed2e595bce5a4e3c81b45712f1fc3882a96
Atakan AKYILDIZ <88201797+atakanakyildizz@users.noreply.github.com>
2024-09-25 14:51:34 +0200

    For Loop basic exercise: counting to our choice.

--- exercise_6_1/exercise_6_1.c
+++ exercise_6_1/exercise_6_1.c
@@ -1,3 +1,16 @@
 //
 // Created by Atakan Akyıldız on 25.09.2024.
 //
+#include <stdio.h>
+
+int main() {
+
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    for (int i = 1; i <= number; i++) {
+        printf("%d\n", i);
+    }
+
+}
