026f9b159d30e2eab8270a5cc98786ca01aa278d
Atakan AKYILDIZ <88201797+atakanakyildizz@users.noreply.github.com>
2024-09-25 14:38:05 +0200

    Coffe-Tea suggestion

--- exercise_5_2/exercise_5_2.c
+++ exercise_5_2/exercise_5_2.c
@@ -1,3 +1,34 @@
+#include <stdio.h>
 //
 // Created by Atakan Akyıldız on 25.09.2024.
 //
+int main() {
+    char choose;
+    int cup_number;
+    printf("Do you drink coffee or tea (c/t)?");
+    scanf("%c", &choose);
+    printf("How many cups do you drink daily: ");
+    scanf("%d", &cup_number);
+
+
+    if (choose=='c') {
+        if ((0 < cup_number) && (cup_number < 2)) {
+            printf("You don't drink a lot of coffee, do you?");
+        }
+        else if ((3 < cup_number) && (cup_number < 20)) {
+            printf("You drink a lot of coffee!");
+        }
+    }
+
+    else if (choose=='t') {
+        if ((0 < cup_number) && (cup_number < 2)) {
+            printf("You do not drink a lot of tea");
+        }
+        else if ((3 < cup_number) && (cup_number < 20)) {
+            printf("You drink a lot of tea!");
+        }
+    }
+    else {
+        printf("An error occurred in the program!");
+    }
+}
