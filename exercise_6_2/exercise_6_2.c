9b7b17e04df15bf2b7952527ecb30df18111c6d3
Atakan AKYILDIZ <88201797+atakanakyildizz@users.noreply.github.com>
2024-09-25 14:56:41 +0200

    "for" loop basic exercise: finding factorial

--- exercise_6_2/exercise_6_2.c
+++ exercise_6_2/exercise_6_2.c
@@ -1,3 +1,19 @@
 //
 // Created by Atakan Akyıldız on 25.09.2024.
 //
+#include <stdio.h>
+
+int main(){
+
+  int number;
+  int sum =1;
+
+  printf("Enter an integer: ");
+  scanf("%d", &number);
+
+  for (int i = 1; i <= number; i++) {
+      sum = i * sum;
+  }
+
+  printf("The factorial of %d is %d", number, sum);
+}
