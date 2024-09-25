5f210d3634588c967d1003567c6cfeb05d159688
Atakan AKYILDIZ <88201797+atakanakyildizz@users.noreply.github.com>
2024-09-25 08:44:18 +0200

    First exercises in C programming

---
+++ exercise_2_1/exercise_2_1.c
@@ -0,0 +1,9 @@
+//
+// Created by Atakan Akyıldız on 25.09.2024.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello everybody !\n");
+
+}
---
+++ exercise_2_2/exercise_2_2.c
@@ -0,0 +1,14 @@
+//
+// Created by Atakan Akyıldız on 25.09.2024.
+//
+#include <stdio.h>
+
+int main(){
+
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    number = number * number;
+    printf("The number is: %d\n", number);
+
+}
