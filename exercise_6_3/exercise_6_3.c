ae15a84118d87425809dbff11bfbf7426969419a
Atakan AKYILDIZ <88201797+atakanakyildizz@users.noreply.github.com>
2024-09-26 09:17:04 +0200

    " do...while" loop basic exercise: finding average score

--- exercise_6_3/exercise_6_3.c
+++ exercise_6_3/exercise_6_3.c
@@ -1,9 +1,27 @@
 //
 // Created by Atakan Akyıldız on 25.09.2024.
 //
+
 #include <stdio.h>
 
 int main(void) {
 
-    printf("Hello, World!\n");
+    float number = 1;
+    float sum = 0;
+    int i = 0;
+
+    printf("The program calculates the average of scores you enter."
+           "\nEnd with a negative integer.\n");
+
+
+    do {
+        printf("Enter score (4-10): ");
+        scanf("%f", &number);
+        sum = sum + number;
+        i++;
+    }while(number>0);
+
+    printf("You entered %d scores.\n", i-1 );
+    printf("The average of scores you enter is %f.\n", sum-number);
+    printf("The average is %.2f\n", (sum-number)/(i-1) );
 }
