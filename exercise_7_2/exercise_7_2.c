e8ef23ac3709e6d8e13e57791e82c714f3177f26
Atakan AKYILDIZ <88201797+atakanakyildizz@users.noreply.github.com>
2024-09-26 09:52:42 +0200

    "functions, switch" basic exercise: calculator

--- exercise_7_2/exercise_7_2.c
+++ exercise_7_2/exercise_7_2.c
@@ -1,3 +1,48 @@
 //
 // Created by Atakan Akyıldız on 25.09.2024.
 //
+#include <stdio.h>
+
+int sum(int a, int b) {
+    return (a + b);
+}
+
+int difference(int a, int b) {
+    return a - b;
+}
+
+int product(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int a, b;
+    int choice =1;
+
+    while (choice > 0) {
+        printf("1: sum of two numbers\n2: difference of two numbers\n"
+       "3: product of two numbers"
+       "\n<0: terminate the program\n");
+        printf("Select calculation: ");
+        scanf("%d", &choice);
+        if (choice < 0) {
+            printf("Terminating the program... ");
+            break;
+        }
+        printf("Enter the first number: ");
+        scanf("%d", &a);
+        printf("Enter the second number: ");
+        scanf("%d", &b);
+        switch (choice) {
+            case 1:
+                printf("%d + %d = %d\n\n,", a, b, sum(a, b));
+            break;
+            case 2:
+                printf("%d - %d = %d\n\n", a, b, difference(a, b));
+            break;
+            case 3:
+                printf("%d * %d = %d\n\n ", a, b, product(a, b));
+            break;
+        }
+    }
+}
