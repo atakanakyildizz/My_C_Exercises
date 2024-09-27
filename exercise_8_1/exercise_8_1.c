7affab0abf7dc36a699d7e4b6d8c426a23190420
Atakan AKYILDIZ <88201797+atakanakyildizz@users.noreply.github.com>
2024-09-27 12:41:49 +0200

    Program for calculate the numbers of hours worked within a specific period and prints the total number of hours, the average length of a day and an itemisation of the hours entered

--- exercise_8_1/exercise_8_1.c
+++ exercise_8_1/exercise_8_1.c
@@ -1,3 +1,29 @@
 //
 // Created by Atakan Akyıldız on 25.09.2024.
 //
+#include <stdio.h>
+
+int main() {
+
+    float total_days;
+    float days;
+    float sum = 0;
+    float array[total_days];
+
+    printf("The program calculates the total hours worked during"
+           "a specific period and the average length of a day.\n");
+    printf("How many days: ");
+    scanf("%f", &total_days);
+     for (int i = 0; i < total_days ; i++) {
+         printf("Enter the working hours for day %d ", i);
+         scanf("%f",&days);
+         array[i] = days;
+         sum = sum + days;
+     }
+    printf("Total hours worked: %.1f\n", sum);
+    printf("Average length of a day: %.1f\nHours entered: ", (sum)/(total_days));
+    for(int i = 0; i < total_days; i++) {
+        printf("%.1f ", array[i]);
+    }
+
+}
